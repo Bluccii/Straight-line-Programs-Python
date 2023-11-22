1082e1ad07fcdf228fb64a90e130cd715da458f7
Ivan Wu <ivanlochoa9@gmail.com>
2023-11-22 13:49:19 -0500

    Initial Commit

--- name_age.py
+++ name_age.py
@@ -2,8 +2,8 @@ import stdio
 import sys
 
 # Accept name (str) and age (str) as command-line arguments.
-name = str(sys.argv[1])
-age = int(sys.argv[2])
+name = str(sys.argv[1]) # Saves first command-line argument as a string variable called "name"
+age = int(sys.argv[2]) # Saves second command-line argument as a integer variable called "age"
 
 # Write the message 'name is age years old.' to standard output.
-stdio.writeln(name)
+stdio.writeln(f"Hi, my name is {name}, and I am {age} year(s) old.")

47db5c79b5b865ccbb10fdcb6617219ca61d839b
Ivan Wu <ivanlochoa9@gmail.com>
2023-11-23 00:18:43 -0500

    Initial Commit

--- gravitational_force.py
+++ gravitational_force.py
@@ -1,4 +1,15 @@
+# Import all necessary modules for the code.
 import stdio
 import sys
 
-...
+# Variables defined via Command-line Argument input from the terminal.
+m1 = float(sys.argv[1])
+m2 = float(sys.argv[2])
+r = float(sys.argv[3])
+G = 6.674e-11
+
+# Calculates the value of gravitational force
+f = G * m1 * m2 / (r ** 2)
+
+# Displays the value of gravitational force as output
+stdio.writeln(f)

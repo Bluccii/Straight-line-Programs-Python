bc7e70a603d80b03f598ffe1d7f667cbe596077d
Bluccii <ivanwubusiness@gmail.com>
2023-11-26 22:50:14 -0500

    Final Code

--- waiting_time.py
+++ waiting_time.py
@@ -1,5 +1,14 @@
+# Import all necessary modules for the code.
 import math
 import stdio
 import sys
 
-...
+# Variables defined via Command-line Argument input from the terminal.
+λ = float(sys.argv[1])
+t = float(sys.argv[2])
+
+# Calculates the probability
+p = math.exp(-λ * t)
+
+# Displays "the probability that one has to wait longer than t" as output
+stdio.writeln(p)
